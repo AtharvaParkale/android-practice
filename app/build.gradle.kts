412fdf05a3507f58fb51ae1f4f798b933b53cc29
atharvaparkale <atharva.parkale@gmail.com>
2026-03-08 23:22:54 +0530

    :memo: add minor changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,9 +6,7 @@ plugins {
 
 android {
     namespace = "com.justonedev.androidpractice"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.justonedev.androidpractice"
