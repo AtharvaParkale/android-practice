106613fc715fd2b95eea13aacc5d9467d335ecb2
atharvaparkale <atharva.parkale@gmail.com>
2026-03-22 21:40:33 +0530

    :memo: git push notes app ui improvements

--- app/src/main/java/com/justonedev/androidpractice/features/notes_app/presentation/NotesScreen.kt
+++ app/src/main/java/com/justonedev/androidpractice/features/notes_app/presentation/NotesScreen.kt
@@ -1,8 +1,8 @@
 package com.justonedev.androidpractice.features.notes_app.presentation
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,21 +10,27 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -34,7 +40,7 @@ import androidx.compose.ui.unit.sp
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun NotesScreen() {
-    val text = remember { mutableStateOf("Hello") }
+    var text by remember { mutableStateOf("Hello") }
     val notes = remember { mutableStateListOf<String>() } // Must be compose state for UI to update
 
 
@@ -51,47 +57,70 @@ fun NotesScreen() {
         )
     }) { paddingValues ->
 
-        Column(
-            modifier = Modifier
-                .padding(paddingValues)
-                .fillMaxSize()
-        ) {
-            TextField(
-                value = text.value,
-                onValueChange = { text.value = it },
-                label = { Text("Note Description") },
+        Box(
+            modifier = Modifier.padding(
+                horizontal = 15.dp,
             )
-            Spacer(Modifier.height(10.dp))
-            Button(onClick = {
-                notes.add(text.value)
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(paddingValues)
+                    .fillMaxSize()
+            ) {
+                TextField(
+                    value = text,
+                    onValueChange = { text = it },
+                    label = { Text("Note Description") },
+                )
+                Spacer(Modifier.height(10.dp))
+                Button(
+                    onClick = {
+                        notes.add(text)
+                        text = ""
+                    },
+                    enabled = text.isNotEmpty(),
+                ) {
+                    Text("Click")
+                }
+                Spacer(Modifier.height(12.dp))
 
-                text.value = ""
-            }) {
-                Text("Click")
-            }
-            Spacer(Modifier.height(12.dp))
+                if (notes.isEmpty()) {
+                    Text("No notes added yet")
 
-            if (notes.isEmpty()) {
-                Text("No notes added yet")
+                    return@Column
+                }
 
-                return@Column
-            }
-
-            LazyColumn(modifier = Modifier.weight(1f)) { // Here always height must be specified
-                items(notes) { note ->
-                    ListItem(
-                        headlineContent = { Text(text = note) },
-                        leadingContent = {
-                            Icon(
-                                imageVector = Icons.Filled.Delete,
-                                contentDescription = "Delete Note",
-                                modifier = Modifier.clickable {
-                                    // handle delete
-                                    notes.remove(note)
-                                },
-                            )
-                        },
-                    )
+                LazyColumn(modifier = Modifier.weight(1f)) {
+                    items(notes) { note ->
+                        ListItem(
+                            modifier = Modifier
+                                .clip(RoundedCornerShape(15.dp))
+                                .padding(
+                                    vertical = 5.dp
+                                ),
+                            headlineContent = {
+                                Text(
+                                    text = note, style = TextStyle(
+                                        color = Color.White, fontWeight = FontWeight.SemiBold
+                                    )
+                                )
+                            },
+                            colors = ListItemDefaults.colors(
+                                containerColor = Color.Gray,
+                                trailingIconColor = Color.Red,
+                                headlineColor = Color.White,
+                            ),
+                            trailingContent = {
+                                Icon(
+                                    imageVector = Icons.Filled.Delete,
+                                    contentDescription = "Delete Note",
+                                    modifier = Modifier.clickable {
+                                        notes.remove(note)
+                                    },
+                                )
+                            },
+                        )
+                    }
                 }
             }
         }
